059293e06b1b8558c62b25352ffce155eeb0e446
vitalylagutin <1873955@gmail.com>
2021-08-13 20:00:49 +0300

    Test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,7 @@ const HomePage = () => {
                 description="Home page's description here" />
             <div className="jumbotron jumbotron-fluid">
                 <div className="container">
-                    <h1 className="display-4">This is the Home Page</h1>
+                    <h1 className="display-4">Here are some changes appeared</h1>
                     <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sed harum voluptatibus aperiam nam, sunt ipsa natus totam, minus cupiditate reiciendis tempora autem ad eius error magnam voluptates quas commodi!</p>
                 </div>
             </div>
