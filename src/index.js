2f36f30be63d7e43f64c96afa4f88caf69b31d31
vitalylagutin <1873955@gmail.com>
2021-08-13 19:24:23 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { hydrate, render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { hydrate, render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
+
 const app = (
     <BrowserRouter>
         <App />
     </BrowserRouter>
-);
+)
 
 rootElement.hasChildNodes()
     ? hydrate(app, rootElement)
